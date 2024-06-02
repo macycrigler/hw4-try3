4fd49f31659cdf8a04acb28c7186a57a795b8e1a
Macy Crigler <macy.crigler@kellogg.northwestern.edu>
2024-06-02 21:12:57 +0000

    finally got the auth stuff to work

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,11 @@
 class EntriesController < ApplicationController
 
+  def index
+    @entries = Entry.where(["user_id" => session["user_id"]])
+  end
+
   def new
+    @user = User.find_by({ "id" => session["user_id"] })
   end
 
   def create
@@ -9,6 +14,9 @@ class EntriesController < ApplicationController
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
+
+    @entry["user_id"] = session["user_id"]
+
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,18 +5,29 @@ class PlacesController < ApplicationController
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    @place = Place.find(params[:id])
+    if @current_user
+      @entries = Entry.where(user_id: @current_user.id, place_id: @place.id)
+    else
+      redirect_to login_path, notice: "You must log in to view this page."
+    end
   end
-
+  
   def new
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
+    @place = Place.new(place_params)
+    if @place.save
+      redirect_to places_path, notice: "Place was successfully created."
+    else
+      render :new
+    end
   end
 
+  private
+
+  def place_params
+    params.require(:place).permit(:name)
+  end
 end
